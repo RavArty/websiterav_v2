1f06871496d48294c72d76d59fe6d2a2ed7445e6
Ravshan <ravkart@gmail.com>
2019-12-17 16:17:58 -0800

    added sass, header and main components

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className="header">
+      <div className="logo">
+        <h2>Logo</h2>
+      </div>
+      <div className="options">
+        <a href="#" className="option">
+          ABOUT
+        </a>
+        <a href="#" className="option">
+          PROJECTS
+        </a>
+        <a href="#" className="option">
+          CONTACT
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
---
+++ src/main/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="main__text-box">
+        <h1 className="main-primary">
+          <span className="main-primary--main">Ravshan Artykov</span>
+          <span className="main-primary--sub">
+            Front End Developer, React, Node
+          </span>
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
