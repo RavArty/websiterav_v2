ba34886ea73c101115de5297368e76ef76974a34
Ravshan <ravkart@gmail.com>
2019-12-23 20:48:36 -0800

    copy email after clicking on button, change tooltip message after copying

--- src/contact/contact.jsx
+++ src/contact/contact.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { ReactComponent as Github } from '../assets/SVG/github-with-circle.svg';
 import { ReactComponent as Facebook } from '../assets/SVG/facebook-with-circle.svg';
 import { ReactComponent as Linkedin } from '../assets/SVG/linkedin-with-circle.svg';
 import { ReactComponent as Email } from '../assets/SVG/mail.svg';
 
 const Contact = () => {
+  const emailText = useRef(null);
+  const toolTip = useRef(null);
+
+  const copyToClipboard = () => {
+    // emailText.current.select();
+    const email = emailText.current.innerText;
+    const elem = document.createElement('textarea');
+    document.body.appendChild(elem);
+    elem.value = email;
+    elem.select();
+    // console.log(emailText.current.innerText);
+    document.execCommand('copy');
+    toolTip.current.innerText = 'Email Copied!';
+    document.body.removeChild(elem);
+  };
+  const outFunc = () => {
+    //console.log('out');
+    toolTip.current.innerText = 'Copy to clipboard';
+  };
+
   return (
     <div className="contact">
       <div className="u-center-text u-margin-bottom-big">
@@ -35,7 +55,21 @@ const Contact = () => {
       </div>
       <div className="contact__email">
         <Email className="contact__email-icon" />
-        <h3>ravkart@gmail.com</h3>
+        <h3 ref={emailText} value="ravkart@gmail.com">
+          ravkart@gmail.com
+        </h3>
+        <div className="tooltip">
+          <button
+            className="contact__email-button"
+            onClick={copyToClipboard}
+            onMouseOut={outFunc}
+          >
+            <span ref={toolTip} className="tooltiptext" id="myTooltip">
+              Copy to clipboard
+            </span>
+            Copy
+          </button>
+        </div>
       </div>
     </div>
   );
