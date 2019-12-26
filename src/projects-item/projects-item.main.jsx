34a3be75ba04d2d56b9a60fbbe9e65cd67570766
Ravshan <ravkart@gmail.com>
2019-12-25 22:03:39 -0800

    main projects hover

--- src/projects-item/projects-item.main.jsx
+++ src/projects-item/projects-item.main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ProjectsItemMain = ({ id, title, imgUrl, caption }) => {
+const ProjectsItemMain = ({ id, title, imgUrl, header, caption }) => {
   return (
     <div className="projects-item__main">
       {/* <div className="image" style={{ backgroundImage: `url(${imgUrl})` }} /> */}
@@ -11,7 +11,7 @@ const ProjectsItemMain = ({ id, title, imgUrl, caption }) => {
           alt="Main Projects"
         ></img>
         <figcaption className="projects-item__main-caption">
-          <h3 className="projects-item__main-caption-header">Header</h3>
+          <h3 className="projects-item__main-caption-header">{header}</h3>
           <p className="projects-item__main-caption-body">{caption}</p>
         </figcaption>
       </figure>
--- src/projects/projects.data.js
+++ src/projects/projects.data.js
@@ -3,25 +3,33 @@ export const PROJECTS_DATA = [
     id: 1,
     title: 'ecom',
     imgUrl: 'https://i.ibb.co/7GW3prF/ecom.png',
-    caption: 'test captioin add something here...'
+    header: 'E-commerce',
+    caption:
+      'A high scalable template for ecommerce websites. Built with React/Redux, Node.js, Express, Firebase.'
   },
   {
     id: 2,
     title: 'colors',
     imgUrl: 'https://i.ibb.co/f2R26L4/colorsv2.png',
-    caption: 'test captioin add something here...'
+    header: 'Colors',
+    caption:
+      'An easy web app that helps designers to isolate specific colors in their images. Built with React, Node.js, and Express for detecting dominant colors on uploaded images.'
   },
   {
     id: 3,
     title: 'rollocoball',
     imgUrl: 'https://i.ibb.co/zfnQxh1/rollocoball.png',
-    caption: 'test captioin add something here...'
+    header: 'Rollocoball',
+    caption:
+      'A 2D tap and play game that challenges the player&#34;s reaction skills. Inspired by the idea of roller coasters in New York, Las Vegas and Los Angeles. Built on Unity Engine (C#)'
   },
   {
     id: 4,
     title: 'turtle_squad',
     imgUrl: 'https://i.ibb.co/GT4Wvk7/turtlesquad.png',
-    caption: 'test captioin add something here...'
+    header: 'Turtle Squad',
+    caption:
+      'Addictive adventure game based on “The 12 Labors Of Hercules”. Starring cute but dangerous turtles. Built on Unity Engine (C#), multiple levels, in-app purchases, and ad-supported game rewards.'
   }
 ];
 
