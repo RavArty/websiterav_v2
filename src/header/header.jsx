576064e53125add0c3666062e9fb847c5919c054
Ravshan <ravkart@gmail.com>
2020-01-17 11:41:18 -0800

    added bootstrap, header

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -9,58 +9,79 @@ const Header = () => {
   };
 
   return (
-    <div className="header">
-      <div className="logo-container">
-        <div className="logo" onClick={scrollToTop}>{`<RA>`}</div>
-        {/* <img src={logo} alt="logo" /> */}
+    <nav className="navbar navbar-expand-lg navbar-dark sticky-top">
+      <a className="navbar-brand" href="#">
+        Navbar
+      </a>
+      <button
+        className="navbar-toggler "
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto float-right">
+          <li className="nav-item mr-5">
+            <Link
+              className="nav-link"
+              activeClass="active"
+              to="about"
+              spy={true}
+              smooth="easeInOutQuint"
+              offset={-40}
+              duration={1000}
+              delay={100}
+            >
+              ABOUT
+            </Link>
+          </li>
+          <li className="nav-item mr-5">
+            <Link
+              className="nav-link"
+              activeClass="active"
+              to="projects"
+              spy={true}
+              smooth="easeInOutQuint"
+              offset={-40}
+              duration={1000}
+              delay={100}
+            >
+              PROJECTS
+            </Link>
+          </li>
+          <li className="nav-item mr-5">
+            <Link
+              className="nav-link"
+              activeClass="active"
+              to="contact"
+              spy={true}
+              smooth="easeInOutQuint"
+              offset={20}
+              duration={1500}
+              delay={100}
+            >
+              CONTACT
+            </Link>
+          </li>
+          <li className="nav-item mr-5">
+            <a
+              href={resume}
+              className="nav-link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              RESUME
+            </a>
+          </li>
+        </ul>
       </div>
-      <div className="options">
-        <Link
-          className="option"
-          activeClass="active"
-          to="about"
-          spy={true}
-          smooth="easeInOutQuint"
-          offset={-40}
-          duration={1000}
-          delay={100}
-        >
-          ABOUT
-        </Link>
-        <Link
-          className="option"
-          activeClass="active"
-          to="projects"
-          spy={true}
-          smooth="easeInOutQuint"
-          offset={-40}
-          duration={1000}
-          delay={100}
-        >
-          PROJECTS
-        </Link>
-        <Link
-          className="option"
-          activeClass="active"
-          to="contact"
-          spy={true}
-          smooth="easeInOutQuint"
-          offset={20}
-          duration={1500}
-          delay={100}
-        >
-          CONTACT
-        </Link>
-        <a
-          href={resume}
-          className="option"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          RESUME
-        </a>
-      </div>
-    </div>
+    </nav>
   );
 };
 
